9c95c0fe849599d9a707693a4faad74bb66c44ea
Henri Oru <henri.oru@gmail.com>
2021-03-09 10:32:03 +0200

    04-segment/

---
+++ Labs/04-segment/display/display.srcs/sim_1/new/tb_hex_7seg.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/03/2021 12:04:20 PM
+-- Design Name: 
+-- Module Name: tb_hex_7seg - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_hex_7seg is
+    Port ( hex_i : in STD_LOGIC;
+           seg_o : out STD_LOGIC);
+end tb_hex_7seg;
+
+architecture Behavioral of tb_hex_7seg is
+
+begin
+
+
+end Behavioral;
