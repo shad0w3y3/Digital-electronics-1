d5a811b6c9cb0154696acefd82cbdf80589d5f7a
Henri Oru <henri.oru@gmail.com>
2021-03-30 12:23:17 +0300

    vivado

--- Labs/07-ffs/flip_flops/flip_flops.srcs/sources_1/new/tb_d_latch.vhd
+++ Labs/07-ffs/flip_flops/flip_flops.srcs/sources_1/new/tb_d_latch.vhd
@@ -32,14 +32,26 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity tb_d_latch is
-    Port ( en : in STD_LOGIC;
-           d : in STD_LOGIC;
-           arst : in STD_LOGIC);
+ --   Port ();
 end tb_d_latch;
 
 architecture Behavioral of tb_d_latch is
 
+signal  s_en :     STD_LOGIC;
+signal  s_d :      STD_LOGIC;
+signal  s_arst :   STD_LOGIC;
+signal  s_q :      STD_LOGIC;
+signal  s_q_bar :  STD_LOGIC
+
 begin
-
-
+    uut_d_latch : entity work.d_latch
+    port map(
+        en  => s_en,
+        d  => s_d,
+        arst => s_arst,
+        q   => s_q,
+        q_bar => s_q_bar
+        
+        );
+          
 end Behavioral;
